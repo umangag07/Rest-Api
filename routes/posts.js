80b529b61ba3c4cbc19aaaaa5dfdf50acf588daa
umang <25umangagarwal@gmail.com>
2020-11-27 20:56:11 +0530

    Sending data when get request is made

--- routes/posts.js
+++ routes/posts.js
@@ -2,26 +2,31 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
 
-router.get('/', (req, res)=>{
-    res.send('we are on post')
+router.get('/',async (req, res)=>{
+    try{
+        const posts = await Post.find();
+        res.json(posts);
+    }catch(err){
+        res.json({message:err})
+    }
 })
 router.get('/specific', (req,res)=>{
     res.send('we are on specific post')
 })
-router.post('/', (req, res)=>{
+router.post('/', async (req, res)=>{
     console.log(req.body)
     const post1 = new Post({
         title:req.body.title,
         description:req.body.description 
     });
 
-    post1.save()
-     .then(data =>{
-         res.json(data)
-     })
-     .catch(err =>{
-         res.json({message:err});
-     })
+    try{
+        const savedPost = await post1.save()
+        res.json(savedPost)
+    }catch(err){
+        res.json({message:err})
+    }
+    
 })
 
 module.exports = router;
