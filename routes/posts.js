38fbd516f0ed6cec11290e99449649f0e0f9f1cb
umang <25umangagarwal@gmail.com>
2020-11-28 00:11:34 +0530

    Added the code to update the post

--- routes/posts.js
+++ routes/posts.js
@@ -56,4 +56,16 @@ router.delete('/:postId', async (req,res)=>{
     }
 })
 
+// Update the post
+router.patch('/:postId', async (req,res)=>{
+    try{
+       const updatePost = await Post.update(
+           {_id:req.params.postId},
+           { $set:{title:req.body.title}}
+        )
+       res.json(updatePost)
+    }catch(err){
+        res.json({message:err}) 
+    }
+})
 module.exports = router;
