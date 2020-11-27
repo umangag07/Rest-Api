ccecc5751ad4344b0ee0b562911b4ae7e9e40193
umang <25umangagarwal@gmail.com>
2020-11-27 23:57:44 +0530

    Added functionality to get the specific post

--- routes/posts.js
+++ routes/posts.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/Post')
 
+// Gets all the post
 router.get('/',async (req, res)=>{
     try{
         const posts = await Post.find();
@@ -10,9 +11,12 @@ router.get('/',async (req, res)=>{
         res.json({message:err})
     }
 })
+
 router.get('/specific', (req,res)=>{
     res.send('we are on specific post')
 })
+
+// Submit a post
 router.post('/', async (req, res)=>{
     console.log(req.body)
     const post1 = new Post({
@@ -23,6 +27,18 @@ router.post('/', async (req, res)=>{
     try{
         const savedPost = await post1.save()
         res.json(savedPost)
+    }catch(err){
+        res.json({message:err})
+    }
+    
+})
+
+//Get a specific post
+router.get('/:postId', async (req,res)=>{
+    //console.log(req.params.postId)
+    try{
+        const S_Post= await Post.findById(req.params.postId)
+        res.json(S_Post)
     }catch(err){
         res.json({message:err})
     }
