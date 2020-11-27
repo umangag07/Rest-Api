f7736151e7fa74af243145464534d6693cdaaa84
umang <25umangagarwal@gmail.com>
2020-11-27 19:49:46 +0530

    Added specific route

--- routes/posts.js
+++ routes/posts.js
@@ -4,5 +4,8 @@ const router = express.Router();
 router.get('/', (req,res)=>{
     res.send('we are on post')
 })
+router.get('/specific', (req,res)=>{
+    res.send('we are on specific post')
+})
 
 module.exports = router;
