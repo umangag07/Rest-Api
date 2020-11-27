ecf83a4516d9bd56b75f3ea93afcd719bacceb9f
umang <25umangagarwal@gmail.com>
2020-11-27 19:30:25 +0530

    connecting to mongodb database

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+//Middlewares(Its like function are execited when routes are being hit)
 
 // Routes
 app.get('/', (req,res)=>{
     res.send('we are on home')
 })
+app.get('/posts', (req,res)=>{
+    res.send('we are on post')
+})
+
+// connect to db
+
+mongoose.connect('mongo "mongodb+srv://cluster0.pi2rv.mongodb.net/Project 0" --nodemonGrack nodemonGrack')
 //How we start listening to the server
 app.listen(3000);
